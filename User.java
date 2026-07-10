1e1d4f49f28a5dc99e7042d5bd2644127071dea4
harutaka[はるたか］ <chuisamfung@gmail.com>
2026-07-10 19:21:25 +0800

    Create User.java

---
+++ User.java
@@ -0,0 +1,38 @@
+public class User
+{
+    private static int id = 100000000;
+    private String username;
+    private Library library = new Library();
+    private String password;
+    private String bio;
+    
+    //Constructors here
+    User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+        id++;
+    }
+    //Getters here
+    public int getId()
+    {
+        return id;
+    }
+    public String getUsername()
+    {
+        return username;
+    }
+    public Library getLibrary()
+    {
+        return library;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+    public String getBio()
+    {
+        return bio;
+    }
+    
+}
