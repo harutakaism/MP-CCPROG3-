1c7efb855578b780fbeb85c8482fd14f452bf87e
harutaka[はるたか］ <chuisamfung@gmail.com>
2026-07-10 19:05:24 +0800

    Update Movies.java

--- Movies.java
+++ Movies.java
@@ -1,2 +1,64 @@
-public class Movies {
+public class Movies
+{
+    private final String title;
+    private static int ID = 800000; //Static to generate unique ID
+    private final String studio;
+    private Rating rating = new Rating(); //Movie Rating
+    private String description;
+    private String status = "Planned";
+    private int durationinmins;
+
+    //Constructor Methods here
+    public Movies(String title, String studio, int durationinmins)
+    {
+        this.title = title;
+        this.studio = studio;
+        this.durationinmins = durationinmins;
+        ID++;
+    }
+    public Movies(String title, String studio, int durationinmins, String status)
+    {
+        this.title = title;
+        this.studio = studio;
+        this.durationinmins = durationinmins;
+        this.status = status;
+        ID++;
+    }
+    public Movies(String title, String studio,String status)
+    {
+        this.title = title;
+        this.studio = studio;
+        this.status = status;
+        ID++;
+    }
+    //Getter Methods here
+    public String getStatus()
+    {
+        return status;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public int getID()
+    {
+        return ID;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getStudio()
+    {
+        return studio;
+    }
+    public Rating getRating()
+    {
+        return rating;
+    }
+    public int getDurationinmins()
+    {
+        return durationinmins;
+    }
 }
+
