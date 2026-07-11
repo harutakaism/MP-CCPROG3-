c3ab67e1a80768b8770006a7735ab5596c065d95
harutaka[はるたか］ <chuisamfung@gmail.com>
2026-07-11 13:28:45 +0800

    Update Movies.java

--- Movies.java
+++ Movies.java
@@ -1,7 +1,8 @@
 public class Movies
 {
     private final String title;
-    private int id = 800000; //Static to generate unique ID
+    private int id = 800000;
+    private static int incre = 0;
     private final String studio;
     private Rating rating = new Rating(); //Movie Rating
     private String description;
@@ -14,7 +15,9 @@ public class Movies
         this.title = title;
         this.studio = studio;
         this.durationinmins = durationinmins;
-        id++;
+        incre++;
+        this.id = id + incre;
+
     }
     public Movies(String title, String studio, int durationinmins, String status)
     {
@@ -22,14 +25,17 @@ public class Movies
         this.studio = studio;
         this.durationinmins = durationinmins;
         this.status = status;
-        id++;
+        incre++;
+        this.id = id + incre;
     }
     public Movies(String title, String studio,String status)
     {
         this.title = title;
         this.studio = studio;
         this.status = status;
-        id++;
+        incre++;
+        this.id = id + incre;
+
     }
     //Getter Methods here
     public String getStatus()
@@ -66,7 +72,11 @@ public class Movies
         System.out.println("Production Studio: " + studio);
         System.out.println("Rating: " + rating.GetOverallrating());
         System.out.println("Duration: ");
-        if(durationinmins >= 60)
+        if(durationinmins < 60)
+        {
+            System.out.println( durationinmins +" Minutes ");
+        }
+        else
         {
             if(durationinmins < 120)
             {
@@ -74,20 +84,25 @@ public class Movies
             }
             else
             {
-                System.out.println( durationinmins / 60 + " Hours ");
+                System.out.print( durationinmins / 60 + " Hours ");
             }
         }
-        if(durationinmins % 60 != 0 )
+        if(durationinmins % 60 != 0 && durationinmins > 60)
         {
             if(durationinmins % 60 == 1)
             {
-                System.out.println(", 1 Minute");
+                System.out.print(", 1 Minute");
             }
             else
             {
-                System.out.println(", " + durationinmins % 60 + " Minutes");
+                System.out.print(", " + durationinmins % 60 + " Minutes");
             }
         }
+        if(durationinmins == 0)
+        {
+            System.out.println("N/A");
+        }
+        System.out.println();
         System.out.println("Status: " + status);
         System.out.println("Description: " + description);
         System.out.println("ID: " + id );
