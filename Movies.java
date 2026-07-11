669529c74f75e99e76b858576348302d9cfab5aa
nickycin <nicky_gendernalik@dlsu.edu.ph>
2026-07-11 15:22:43 +0800

    Update Movies.java

--- Movies.java
+++ Movies.java
@@ -107,6 +107,25 @@ public class Movies
         System.out.println("Description: " + description);
         System.out.println("ID: " + id );
     }
+    public void setStatus(String status)
+    {
+        if(status.equalsIgnoreCase("Planned"))
+        {
+            this.status = "Planned";
+        }
+        else if(status.equalsIgnoreCase("In Progress"))
+        {
+            this.status = "In Progress";
+        }
+        else if(status.equalsIgnoreCase("Completed"))
+        {
+            this.status = "Completed";
+        }
+        else
+        {
+            System.out.println("Invalid status. Status remains as" + this.status);
+        }
+    }
 }
 
 
