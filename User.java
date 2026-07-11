3984fb6cf26ab374976050c3e5751fdecba88e18
harutaka[はるたか］ <chuisamfung@gmail.com>
2026-07-11 20:11:27 +0800

    Update User.java - With Javadoc

--- User.java
+++ User.java
@@ -7,7 +7,12 @@ public class User
     private String password;
     private String bio;
 
-    //Constructors here
+    /**
+     * Creates a new User with username and password as parameter.
+     *
+     * @param username is user's chosen username
+     * @param password is user's chosen password
+     */
     User(String username, String password)
     {
         this.username = username;
@@ -15,23 +20,48 @@ public class User
         incre++;
         this.id = id + incre;
     }
-    //Getters here
+
+    /**
+     * Getter Method that returns the ID
+     *
+     * @return the number for the ID of a User
+     */
     public int getId()
     {
         return id;
     }
+    /**
+     * Getter Method that returns the Username
+     *
+     * @return the string of the user's Username
+     */
     public String getUsername()
     {
         return username;
     }
+    /**
+     * Getter Method that returns the Library
+     *
+     * @return the library of the user that holds its saved media pieces.
+     */
     public Library getLibrary()
     {
         return library;
     }
+    /**
+     * Getter Method that returns the Password
+     *
+     * @return the String password of a user
+     */
     public String getPassword()
     {
         return password;
     }
+    /**
+     * Getter Method that returns the Biography of a user
+     *
+     * @return the string of a user's Bio
+     */
     public String getBio()
     {
         return bio;
