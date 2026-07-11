c872fd3f8a78dbe83d2172d22d09da3ab3e9d156
harutaka[はるたか］ <chuisamfung@gmail.com>
2026-07-11 20:01:13 +0800

    Update Rating.java - With Javadoc

--- Rating.java
+++ Rating.java
@@ -5,8 +5,11 @@ public class Rating
     private int sum;
     private int recentrate;
     private String review;
-
-
+    
+    /**
+     * Constructor that creates the ratings to be used for the media types
+     *
+     */
     public Rating()
     {
         numberofratings = 0;
@@ -15,37 +18,70 @@ public class Rating
         recentrate = 0;
         review = "";
     }
-
-    //Getter Methods
-
+    /**
+     * Getter Method that returns the number of ratings
+     *
+     * @return ther number of ratings a piece has recieved.
+     */
     public int getNumberOfRatings()
     {
         return numberofratings;
     }
+    /**
+     * Getter Method that returns the average rating
+     *
+     * @return the average rating of a piece.
+     */
     public float GetOverallrating()
     {
         return overallrating;
     }
+    /**
+     * Getter Method that returns the rating score recently
+     *
+     * @return the recently given rating
+     */
     public int getRecentrate()
     {
         return recentrate;
     }
+    /**
+     * Getter Method that returns the review of a piece.
+     *
+     * @return the string that contains the review of a piece.
+     */
     public String getReview()
     {
         return review;
     }
+
+    /**
+     * Method that checks if a media piece has a rating or not.
+     *
+     * @return the boolean value whether it has a rating or not.
+     */
     public boolean hasRating()
     {
         return numberofratings > 0;
     }
+
+    /**
+     * Setter Method that sets the string for review
+     *
+     * @param review the review to add to the media piece during rating period.
+     */
     public void setReview(String review)
     {
         this.review = review;
         System.out.println("Review added!");
     }
 
-    //Setter Methods???
-
+     /**
+     * Allows the user to rate a specific media piece by passing its rating as a parameter and contribute it to
+     * the overall rating of the media piece.
+     * 
+     * @param UserRate the users given rating on a specific media piece
+     */
     public void Rate(int UserRate)
     {
         if(UserRate > 10 || UserRate < 1)
