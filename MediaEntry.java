e39cd6db5b0a4903f6324a9b57e8abcf7f7dc17d
nickycin <nicky_gendernalik@dlsu.edu.ph>
2026-07-30 23:42:59 +0800

    Added MediaEntry class

---
+++ MediaEntry.java
@@ -0,0 +1,12 @@
+public abstract class MediaEntry
+{
+    public abstract int getID();
+
+    public abstract String getTitle();
+
+    public abstract String getStatus();
+
+    public abstract void displayInfo();
+
+    public abstract String getMediaType();
+}
